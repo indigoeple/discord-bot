3bd67c52334285455001a596e1276fead67173e0
indigoeple <indigoeple@users.noreply.github.com>
2017-08-18 23:59:51 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,16 +6,16 @@ import os
 
 client = discord.Client()
 
-#client.event
+@client.event
 async def on_ready():
-  print("logged in as")
+  print('Logged in as')
   print(client.user.name)
   print(client.user.id)
-  print("------")
+  print('------')
   
-#client.event
+@client.event
 async def on_message(message):
-  if message.content.startswith("!shout"):
-    await client.send_message(message.channel, "SHOUT OUT LOUD!")
+  if message.content.startswith('!shout'):
+    await client.send_message(message.channel, 'SHOUT OUT LOUD!')
 
-client.run("MzQ4MzQxMjUxNTU4MzQyNjc2.DHlmsA.x5VmA1kjQxa02NZ5hHV2RiXmHBU")
+client.run('MzQ4MzQxMjUxNTU4MzQyNjc2.DHlwJg.Rlb1QsUhLjXbHCPc3q9YHj9KixQ')
